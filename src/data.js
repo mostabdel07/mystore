b2c2c012841efa21e3f92301297e231487c8864c
mostabr7 <mostabdel07@gmail.com>
2020-03-26 03:45:02 +0100

    Product compenent complete

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Nike Jordan - Black/Red",
+    img: "img/jordan.png",
+    price: 180,
+    company: "NIKE",
+    info:
+      "Ahora, el Modo Retrato es aún mejor gracias al segundo objetivo de la cámara. Los fondos se atenúan formando un desenfoque con un toque artístico, lo que aporta a tus fotos una calidad similar a la de las cámaras réflex digitales.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Nike Air - White/Blue",
+    img: "img/adidas.png",
+    price: 120,
+    company: "NIKE",
+    info:
+      "Ahora, el Modo Retrato es aún mejor gracias al segundo objetivo de la cámara. Los fondos se atenúan formando un desenfoque con un toque artístico, lo que aporta a tus fotos una calidad similar a la de las cámaras réflex digitales.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 2,
+  title: "Google Pixel - Black",
+  img: "img/jordan.png",
+  price: 10,
+  company: "GOOGLE",
+  info:
+    "Ahora, el Modo Retrato es aún mejor gracias al segundo objetivo de la cámara. Los fondos se atenúan formando un desenfoque con un toque artístico, lo que aporta a tus fotos una calidad similar a la de las cámaras réflex digitales.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
